070b4def04143fbf32bd89d53111e7038b36dd66
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-08-02 14:27:45 +0200

    Modification Profil and Password / form, template & Controller

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\EditProfileType;
+use App\Form\EditProfilType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+/**
+ * @Route("/profil", name="profil_")
+ */
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/modifier", name="modify")
+     */
+    public function modify(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis a jour');
+            return $this->redirectToRoute('profil_home');
+        }
+        return $this->render('profil/modify.html.twig',[
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/modifyPassword", name="modifyPassword")
+     */
+    public function modifyPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if($request->isMethod("POST")){
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            //Verify if 2 passwords are the same
+            if($request->request->get('pass') == $request->request->get('passConf')){
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message','Mot de passe mis a jour avec succes');
+                return $this->redirectToRoute('profil_home');
+            }else{
+                $this->addFlash('message', "Les deux mots de passe ne sont pas identiques");
+            }
+        }
+        return $this->render('profil/modifyPassword.html.twig');
+    }
+
+
+    /**
+     * @Route("/supprimer/{id}", name="delete")
+     */
+    public function delete($id, UserRepository $userRepository ):RedirectResponse
+    {
+        $user = $userRepository->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success','Votre compte à bien été supprimé');
+        return $this->redirectToRoute('main_home');
+    }
+}
---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('campus')
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
