7ea0c2f051a14f8f43953744ec2053549e0581ad
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-08-02 14:26:40 +0200

    redirection Home and config Home

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/",name="main_")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
