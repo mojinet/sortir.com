ae6fb3094f0999437eff25a2952a9baa85343d8e
Niiks01 <nicolas_clochette@yahoo.fr>
2021-08-03 12:58:24 +0200

    Details sortie ( add SortieController and details.html.twig )

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/sortie/details/{id}", name="sortie_details")
+     */
+    public function details(int $id, SortieRepository $sortieRepository): Response
+    {
+        $sortie = $sortieRepository->find($id);
+
+        return $this->render('sortie/details.html.twig', [
+
+            "sortie" => $sortie
+
+        ]);
+    }
+}
