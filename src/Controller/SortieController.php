a4e7e0639fc9b1ac1e852f9fa26e796098c9790c
Niiks01 <nicolas_clochette@yahoo.fr>
2021-08-04 15:47:03 +0200

    add creation page ( with form ) and add flash message success

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +27,33 @@ class SortieController extends AbstractController
 
             "sortie" => $sortie
 
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/create", name="sortie_create")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted()){
+
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La sortie a bien été créer');
+            return $this->redirectToRoute('sortie_details', ['id' => $sortie->getId()]);
+        }
+
+        //todo traiter le formulaire
+
+        return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm->createView()
         ]);
     }
 }
---
+++ src/Form/SortieType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('dateHeureDebut', DateType::class, [
+                'label' => 'Date et heure de la sortie',
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => 'Date limite inscription',
+                'html5' => true,
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => 'Nombre de place',
+
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée',
+
+            ])
+            ->add('infosSortie', TextareaType::class,[
+                'label' => 'Description et infos de la sortie'
+            ])
+            ->add('campus', EntityType::class,[
+                'class'=> Campus::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('ville', EntityType::class, [
+                'class'=> Ville::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('lieu', EntityType::class, [
+                'class'=> Lieu::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('rue', TextType::class, [
+                'label' => 'rue',
+                'mapped' => false
+            ])
+            ->add('code_postal', TextType::class, [
+                'label' => 'code postal',
+                'mapped' => false
+            ])
+            ->add('latitude', NumberType::class, [
+                'label' => 'latitude',
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('longitude', NumberType::class, [
+                'label' => 'longitude',
+                'required' => false,
+                'mapped' => false
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
