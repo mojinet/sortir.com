3ae0e0f8234410060cd0bc693414bd77cf007e54
Modji <jeanbaptiste.cochinard@gmail.com>
2021-08-05 10:14:11 +0200

    add LieuType/VilleType & associate page for create lieu or ville

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use App\Form\LieuType;
+use App\Form\VilleType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 
 class SortieController extends AbstractController
 {
@@ -18,11 +23,52 @@ class SortieController extends AbstractController
     {
         $sortie = $sortieRepository->find($id);
 
-
         return $this->render('sortie/details.html.twig', [
 
             "sortie" => $sortie
 
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/ajouter/ville", name="sortie_ajouter_ville")
+     */
+    public function ajouterVille(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $ville = new Ville();
+        $villeForm = $this->createForm(VilleType::class,$ville);
+        $villeForm->handleRequest($request);
+
+        if($villeForm->isSubmitted() && $villeForm->isValid()){
+            $entityManager->persist($ville);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('sortie/ajouterVille.html.twig',[
+            'villeForm' => $villeForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/ajouter/lieu", name="sortie_ajouter_lieu")
+     */
+    public function ajouterLieu(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class,$lieu);
+        $lieuForm->handleRequest($request);
+
+        if($lieuForm->isSubmitted()){
+            $entityManager->persist($lieu);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('sortie/ajouterLieu.html.twig',[
+            'lieuForm' => $lieuForm->createView()
         ]);
     }
 }
